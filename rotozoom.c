23862842d2d9da7ea9631ce57213a97c3401a14b
Mika Tuupola <tuupola@appelsiini.net>
2023-03-17 14:31:08 +0700

    Apply astyle formatting (#9)
    $ astyle --options=.astylerc --recursive "*.c" "*.h"

--- rotozoom.c
+++ rotozoom.c
@@ -38,7 +38,8 @@ static uint16_t angle;
 // static float sinlut[360];
 // static float coslut[360];
 
-void rotozoom_init()
+void
+rotozoom_init()
 {
     /* Generate look up tables. */
     // for (uint16_t i = 0; i < 360; i++) {
@@ -47,7 +48,8 @@ void rotozoom_init()
     // }
 }
 
-void rotozoom_render(hagl_backend_t const *display)
+void
+rotozoom_render(hagl_backend_t const *display)
 {
     float s, c, z;
 
@@ -68,7 +70,7 @@ void rotozoom_render(hagl_backend_t const *display)
             if (v < 0) {
                 v += HEAD_HEIGHT;
             }
-            hagl_color_t *color = (hagl_color_t*) (head + HEAD_WIDTH * sizeof(hagl_color_t) * v + sizeof(hagl_color_t) * u);
+            hagl_color_t *color = (hagl_color_t *) (head + HEAD_WIDTH * sizeof(hagl_color_t) * v + sizeof(hagl_color_t) * u);
 
             if (1 == PIXEL_SIZE) {
                 hagl_put_pixel(display, x, y, *color);
@@ -80,7 +82,8 @@ void rotozoom_render(hagl_backend_t const *display)
     }
 }
 
-void rotozoom_animate()
+void
+rotozoom_animate()
 {
     angle = (angle + SPEED) % 360;
 }
